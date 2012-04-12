448068a7f67b4c6620525576e4b6a0e523462f44
librarymixer <admin@librarymixer.com>
2012-04-11 21:46:32 -0700

    Version bump to 2012041102

--- MixologistGui/version.h
+++ MixologistGui/version.h
@@ -20,7 +20,7 @@
 
 #include <QString>
 
-#define VERSION Q_UINT64_C(2012041101)
+#define VERSION Q_UINT64_C(2012041102)
 
 namespace VersionUtil {
     QString inline convert_to_display_version(qlonglong inputVersion) {
